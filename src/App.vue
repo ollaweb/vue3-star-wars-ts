2b7d66977711fc298bd59d4236583cda3f1bf2f0
Olga L <olla.bradford@gmail.com>
2022-05-31 15:35:21 +0300

    Getting data from API with Servises

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="wrapper">
+    <div class="container">
+      <Header />
+      <main class="wrapper-content">
+        <router-view />
+      </main>
+      <Footer />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Header from '@/components/Header.vue'
+import Footer from '@/components/Footer.vue'
+
+export default {
+  components: { Header, Footer }
+}
+</script>
+
+<style></style>
---
+++ src/pages/home.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>Home page</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent, onMounted } from 'vue'
+import DataService from '@/services/DataServise'
+import ResponseData from '@/types/ResponseData'
+
+export default defineComponent({
+  setup() {
+    onMounted(() => {
+      getPeople()
+    })
+
+    const getPeople = () => {
+      DataService.getAll()
+        .then((res: ResponseData) => console.log(res.data))
+        .catch((e: Error) => console.log(e))
+    }
+  }
+})
+</script>
