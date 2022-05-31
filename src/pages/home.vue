5116bcbfe204cbc3ab88346881ccfaeba57709c1
Olga L <olla.bradford@gmail.com>
2022-05-31 16:19:51 +0300

    Added preloader and render data from api

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,23 +1,59 @@
 <template>
-  <h1>Home page</h1>
+  <div style="text-align: center; margin: 2rem 0">
+    <h1 class="title">Star Wars</h1>
+    <p>
+      Type Script, Services, Composition API, REST API
+      <router-link class="link" to="/about">and more</router-link>
+    </p>
+  </div>
+  <div v-if="loading">
+    <Spinner />
+  </div>
+  <div v-else>
+    <table>
+      <thead>
+        <td>Name</td>
+        <td>Gender</td>
+        <td>Mass</td>
+      </thead>
+      <tbody>
+        <tr v-for="(person, index) in people.results" :key="index">
+          <td>{{ person.name }}</td>
+          <td>{{ person.gender }}</td>
+          <td>{{ person.mass }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted } from 'vue'
+import { defineComponent, ref, onMounted } from 'vue'
 import DataService from '@/services/DataServise'
 import ResponseData from '@/types/ResponseData'
+import People from '@/types/People'
+
+import Spinner from '@/components/UI/Spinner.vue'
 
 export default defineComponent({
+  components: { Spinner },
   setup() {
+    const loading = ref(true as Boolean)
+    const people = ref({} as People)
     onMounted(() => {
       getPeople()
     })
 
     const getPeople = () => {
       DataService.getAll()
-        .then((res: ResponseData) => console.log(res.data))
+        .then((res: ResponseData) => {
+          people.value = res.data
+          loading.value = false
+        })
         .catch((e: Error) => console.log(e))
     }
+
+    return { loading, people }
   }
 })
 </script>
---
+++ src/types/People.tsx
@@ -0,0 +1,6 @@
+export default interface People{
+  count: Number,
+  next: any,
+  previous: any,
+  result: Array<Object>
+}
